2f451e5559399fb61a906b23520d593cacc9ba28
kevlarmusik <kevlar.eq@gmail.com>
2013-09-04 13:13:12 -0400

    Step 4 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Photogur::Application.routes.draw do
-  resources :pictures, :only => [:index, :show]
+
+  get 'pictures' => 'pictures#index'
+
+  get 'pictures/0' => 'pictures#picture0'
+  get 'pictures/1' => 'pictures#picture1'
+  get 'pictures/2' => 'pictures#picture2'
+
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,4 +64,4 @@ Photogur::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
