8af4d12080bd4a3fa8af34f9d08ad6c21c9e5965
kevlarmusik <kevlar.eq@gmail.com>
2013-09-05 18:09:58 -0400

    Added devise for user login and linked user id to pictures uploaded

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ActiveRecord::Base
   attr_accessible :artist, :title, :url, :id
+
+  belongs_to :user
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Photogur::Application.routes.draw do
+  devise_for :users
+
   resources :pictures
   root :to => "pictures#index"
 end
